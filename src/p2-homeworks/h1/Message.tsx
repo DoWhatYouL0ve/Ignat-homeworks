62063b6653510b0f2a0bee70e30c01d361077bd6
DoWhatYouL0ve <william_ross@mailfence.com>
2022-04-04 11:24:29 +0200

    Initial commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type MessagePropsType = {
+    avatar: any
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
