c3c13be0dcf2b6e314c7e5c9a07a749be788d9ec
DoWhatYouL0ve <william_ross@mailfence.com>
2022-05-01 01:33:59 +0200

    5th homework + GH pages updated

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {PATH} from './Pages'
+import style from './style.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={style.navContainer}>
+            <nav className={style.hidden}>
+                <NavLink to={PATH.PRE_JUNIOR} className={style.navLinks} style={({ isActive }) =>
+                    (isActive ? {color: 'red'} : {color: 'grey'})}>
+                    Pre-Junior
+                </NavLink>
+                <NavLink to={PATH.JUNIOR} className={style.navLinks} style={({ isActive }) =>
+                    (isActive ? {color: 'red'} : {color: 'grey'})}>
+                    Junior
+                </NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={style.navLinks} style={({ isActive }) =>
+                    (isActive ? {color: 'red'} : {color: 'grey'})}>
+                    Junior+
+                </NavLink>
+                <div className={style.clicker}>x</div>
+            </nav>
 
         </div>
     )
