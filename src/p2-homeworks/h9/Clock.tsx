a67dec09e967be12f2a895f932589ce36d0c1e1b
DoWhatYouL0ve <william_ross@mailfence.com>
2022-05-28 20:57:12 +0200

    9th homework + GH pages

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,50 +1,51 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import styles from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = +setInterval(() => {
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = (date.getHours() > 9 ? date.getHours() : '0' + date.getHours() )+ ':' + (date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes() )+ ':' + (date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds())// fix with date
+    const stringDate = date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() // fix with date
 
     return (
-        <div>
-            <div
+        <div className={styles.clockContainer}>
+            <div className={styles.timeContainer}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
-
+            <br/>
             {show && (
-                <div>
+                <div className={styles.dateContainer}>
                     {stringDate}
                 </div>
             )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            <div>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
